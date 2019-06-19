c8a70e77356c9133ac0d0ea19434266633b2549b
ShyGuyPy <luke.vawter1@gmail.com>
2019-06-19 17:28:51 -0400

    playing with class instantiation

--- test.js
+++ test.js
@@ -1,6 +1,21 @@
+//
+// function test_func(n1,n2) {
+//   return n1 * n2
+// }
 
-function test_func(n1,n2) {
-  return n1 * n2
+class test_toy{
+  constructor(height, width){
+    this.height = height;
+    this.width = width;
+  }
+
+    stateParameters() {
+      alert(`my test_toy object is ${this.width} wide and ${this.height} tall`);
+    }
 }
 
-alert(test_func(7,3))
+let test = new test_toy(10,12);
+
+test.stateParameters();
+
+//alert(test_func(7,3))
